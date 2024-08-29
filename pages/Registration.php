f5424a2842515f41896c4afd02cbb2b1254aa308
Alexmagana5885 <84841059+Alexmagana5885@users.noreply.github.com>
2024-08-29 16:30:41 +0300

    commit

--- pages/Registration.php
+++ pages/Registration.php
@@ -7,283 +7,10 @@
     <title>Registration Form</title>
     <link href="assets/img/favicon.png" rel="icon">
     <link href="assets/img/favicon.png" rel="favicon.png">
+    <link rel="stylesheet" href="../assets/CSS/registration.css">
 
 </head>
 
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        background-color: #f4f4f4;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 90%;
-        margin: 0;
-        padding: 0;
-    }
-
-    .container {
-        background: #fff;
-        padding: 20px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        width: 70%;
-        height: 90%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        overflow-y: auto;
-    }
-
-    /* Large screens (desktops) */
-    @media (min-width: 1024px) {
-        .container {
-            width: 70%;
-            height: 90%;
-        }
-    }
-
-    /* Small screens (tablets and phones) */
-    @media (max-width: 1024px) {
-        .container {
-            width: 90%;
-            height: auto;
-            padding: 15px;
-            margin-top: 40px;
-        }
-
-    }
-
-    h2 {
-        text-align: center;
-        margin-bottom: 20px;
-    }
-
-    .progress-bar {
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 30px;
-    }
-
-    .progress-step {
-        width: 25%;
-        text-align: center;
-        position: relative;
-    }
-
-    .progress-step::before {
-        content: '';
-        position: absolute;
-        top: 10px;
-        left: 0;
-        right: 0;
-        height: 3px;
-        background: #e0e0e0;
-        z-index: -1;
-    }
-
-    .progress-step.active::before {
-        background: #3498db;
-    }
-
-    .progress-step-circle {
-        width: 20px;
-        height: 20px;
-        background: #e0e0e0;
-        border-radius: 50%;
-        display: inline-block;
-        line-height: 20px;
-        text-align: center;
-        color: #fff;
-    }
-
-    .progress-step.active .progress-step-circle {
-        background: #3498db;
-    }
-
-    .form-step {
-        display: none;
-    }
-
-    .form-step.active {
-        display: block;
-    }
-
-    .form-navigation {
-        display: flex;
-        justify-content: space-between;
-        margin-top: auto;
-    }
-
-    .form-navigation button {
-        padding: 10px 20px;
-        background: #3498db;
-        border: none;
-        color: #fff;
-        cursor: pointer;
-        flex-grow: 1;
-        margin: 5px;
-        border-radius: 5px;
-    }
-
-    .form-navigation button:disabled {
-        background: #e0e0e0;
-        cursor: not-allowed;
-    }
-
-    .form-navigation button.previous {
-        background: #555;
-    }
-
-    /* General styles for labels */
-    label {
-        display: block;
-        margin-bottom: 8px;
-        font-weight: bold;
-        color: #333;
-    }
-
-    /* General styles for input fields */
-    input[type="text"],
-    input[type="email"],
-    input[type="tel"],
-    input[type="date"],
-    input[type="number"],
-    input[type="file"],
-    input[type="month"],
-    input[type="password"],
-    select {
-        width: calc(100% - 22px);
-        padding: 8px;
-        margin-bottom: 15px;
-        border: 0;
-        border-radius: 4px;
-        box-sizing: border-box;
-        box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2);
-
-    }
-
-    textarea {
-        width: calc(100% - 22px);
-
-        padding: 10px;
-        margin-bottom: 15px;
-        border: 0;
-        border-radius: 4px;
-        box-sizing: border-box;
-        resize: vertical;
-        box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2);
-
-    }
-
-
-    input[type="file"] {
-        padding: 0;
-        border: none;
-    }
-
-
-    .form-stepINdiv {
-        display: flex;
-        justify-content: space-between;
-
-    }
-
-    .stepINdivdiv {
-        width: 50%;
-    }
-
-    /* Responsive adjustments */
-    @media (max-width: 1024px) {
-        label {
-            font-size: 14px;
-
-        }
-
-        input[type="text"],
-        input[type="email"],
-        input[type="tel"],
-        input[type="date"],
-        input[type="number"],
-        input[type="file"],
-        input[type="month"],
-        input[type="password"],
-        select {
-            padding: 8px;
-        }
-
-        textarea {
-            padding: 8px;
-        }
-
-        .form-stepINdiv {
-
-            flex-direction: column;
-
-        }
-
-        .stepINdivdiv {
-            width: 100%;
-
-        }
-    }
-
-
-    .password-container {
-        position: relative;
-        display: flex;
-        align-items: center;
-        width: 100%;
-    }
-
-
-    .password-container input {
-        width: 100%;
-        padding: 10px;
-        padding-right: 40px;
-        box-sizing: border-box;
-        border: 0;
-        border-radius: 4px;
-        box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2);
-    }
-
-
-    .password-container .toggle-icon {
-        position: absolute;
-        right: 10px;
-        cursor: pointer;
-        font-size: 14px;
-        color: #333;
-    }
-
-    .error-message {
-        color: red;
-        font-size: 13px;
-        display: none;
-    }
-
-    .stepINdivdiv {
-        margin: 10px;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .payment-button {
-        background-color: #007BFF;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        padding: 10px 20px;
-        margin: 5px;
-        cursor: pointer;
-        font-size: 16px;
-        width: 60%;
-    }
-
-    .payment-button:hover {
-        background-color: #0056b3;
-
-    }
-</style>
 
 <body>
 
@@ -445,120 +172,6 @@
         </div>
     </form>
 
-    <style>
-        /* General Popup Container Styling */
-        .popup-container {
-            display: none;
-            position: fixed;
-            z-index: 1000;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100vh;
-            /* overflow: auto; */
-            background-color: rgba(0, 0, 0, 0.5);
-
-        }
-
-        .popup-content {
-            background-color: #fff;
-            margin: 8% auto;
-            padding: 20px;
-            border: 1px solid #888;
-            width: 80%;
-            max-width: 500px;
-            border-radius: 8px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-            position: relative;
-        }
-
-        /* Close Button Styling */
-        .close {
-            color: #aaa;
-            float: right;
-            font-size: 35px;
-            font-weight: bold;
-            cursor: pointer;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: black;
-            text-decoration: none;
-            cursor: pointer;
-        }
-
-
-        .popup-logo {
-            display: block;
-            margin: 0 auto 15px;
-            max-width: 150px;
-
-        }
-
-        form {
-            display: flex;
-            flex-direction: column;
-        }
-
-
-        label {
-            margin: 10px 0 5px;
-        }
-
-        input[type="text"] {
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            margin-bottom: 15px;
-        }
-
-        input[type="text"]:focus {
-            border-color: #007bff;
-            outline: none;
-        }
-
-        .payButtons {
-            display: flex;
-            justify-content: space-between;
-            margin: 0 auto;
-        }
-
-        .payButtons button {
-            padding: 10px 20px;
-            border: none;
-            border-radius: 4px;
-            color: #fff;
-            cursor: pointer;
-            font-size: 16px;
-        }
-
-        .pay-btn {
-            background-color: #28a745;
-
-        }
-
-        .cancel-btn {
-            background-color: #dc3545;
-
-        }
-
-        .pay-btn:hover {
-            background-color: #218838;
-        }
-
-        .cancel-btn:hover {
-            background-color: #c82333;
-        }
-
-
-        @media (max-width: 600px) {
-            .popup-content {
-                width: 90%;
-                margin: 10% auto;
-            }
-        }
-    </style>
 
 
     <!-- M-Pesa Payment Form -->
@@ -570,7 +183,7 @@
             <input type="number" id="phone-number-mpesa" name="phone-number" placeholder="Enter your phone number">
             <label for="amount">Amount</label>
             <input type="text" id="amount" name="amount" value="300.00" readonly>
-            <p>Confirm that you are making a payment of 300 Ksh as membership fees to the Association of Governmental
+            <p>Confirm that you are making a payment of 300 Ksh as membership fees to the Association of Government
                 Librarians.</p>
             <div class="payButtons">
                 <button class="pay-btn" id="MakePaymentBTN" type="submit">Make Payment</button>
@@ -626,11 +239,109 @@
         </div>
     </div>
 
-
-
-
-
     <script>
+        const steps = document.querySelectorAll(".form-step");
+        const nextBtn = document.querySelector(".next");
+        const prevBtn = document.querySelector(".previous");
+        const progressSteps = document.querySelectorAll(".progress-step");
+
+        let currentStep = 0;
+
+        function updateFormStep() {
+            steps.forEach((step, index) => {
+                step.classList.toggle("active", index === currentStep);
+            });
+            progressSteps.forEach((step, index) => {
+                step.classList.toggle("active", index <= currentStep);
+            });
+
+            prevBtn.disabled = currentStep === 0;
+            nextBtn.textContent = currentStep === steps.length - 1 ? "Submit" : "Next";
+        }
+
+        nextBtn.addEventListener("click", () => {
+            if (currentStep < steps.length - 1) {
+                currentStep++;
+                updateFormStep();
+            } else {
+                // Check if all required fields are filled out
+                const form = document.getElementById("registrationForm");
+                if (form.checkValidity()) {
+                    // Form is valid, submit it
+                    form.submit();
+                    alert("Registration submitted successfully. Wait for the reply");
+                } else {
+                    // Form is not valid, display errors
+                    form.reportValidity();
+                }
+            }
+        });
+
+        prevBtn.addEventListener("click", () => {
+            if (currentStep > 0) {
+                currentStep--;
+                updateFormStep();
+            }
+        });
+
+        updateFormStep();
+
+        // JavaScript for handling the payment popups
+        const mpesaForm = document.getElementById('mpesaForm');
+        const paypalPopup = document.getElementById('paypalPopup');
+        const cardPopup = document.getElementById('cardPopup');
+
+        const mpesaBtn = document.getElementById('mpesa');
+        const paypalBtn = document.getElementById('paypal');
+        const cardBtn = document.getElementById('card');
+
+        const closeBtns = document.querySelectorAll('.close');
+
+        function openPopup(popup) {
+            popup.style.display = 'block';
+        }
+
+        function closeAllPopups() {
+            mpesaForm.style.display = 'none';
+            paypalPopup.style.display = 'none';
+            cardPopup.style.display = 'none';
+        }
+
+        mpesaBtn.addEventListener('click', () => openPopup(mpesaForm));
+        paypalBtn.addEventListener('click', () => openPopup(paypalPopup));
+        cardBtn.addEventListener('click', () => openPopup(cardPopup));
+
+        closeBtns.forEach(button => {
+            button.addEventListener('click', closeAllPopups);
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target.classList.contains('popup-container')) {
+                closeAllPopups();
+            }
+        });
+
+        function togglePassword(id) {
+            const passwordField = document.getElementById(id);
+            passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
+        }
+
+        function validatePasswords() {
+            const password = document.getElementById('password').value;
+            const confirmPassword = document.getElementById('confirm-password').value;
+            const errorMessage = document.getElementById('error-message');
+
+            if (password !== confirmPassword) {
+                errorMessage.style.display = 'block';
+            } else {
+                errorMessage.style.display = 'none';
+            }
+        }
+    </script>
+
+
+
+    <!-- <script>
 
         const steps = document.querySelectorAll(".form-step");
         const nextBtn = document.querySelector(".next");
@@ -656,7 +367,6 @@
                 currentStep++;
                 updateFormStep();
             } else {
-                // Submit form here
                 
                 document.getElementById("registrationForm").submit();
                 alert("Registration submitted successfully. Wait for the reply");
@@ -672,43 +382,34 @@
 
         updateFormStep();
 
-        // Show/Hide card payment details based on selection
-        // Get the elements
         const mpesaForm = document.getElementById('mpesaForm');
         const paypalPopup = document.getElementById('paypalPopup');
         const cardPopup = document.getElementById('cardPopup');
 
-        // Get the buttons or triggers to open popups
         const mpesaBtn = document.getElementById('mpesa');
         const paypalBtn = document.getElementById('paypal');
         const cardBtn = document.getElementById('card');
 
-        // Get the close buttons
         const closeBtns = document.querySelectorAll('.close');
 
-        // Function to open a popup
         function openPopup(popup) {
             popup.style.display = 'block';
         }
 
-        // Function to close all popups
         function closeAllPopups() {
             mpesaForm.style.display = 'none';
             paypalPopup.style.display = 'none';
             cardPopup.style.display = 'none';
         }
-
-        // Event listeners for open buttons
+ons
         mpesaBtn.addEventListener('click', () => openPopup(mpesaForm));
         paypalBtn.addEventListener('click', () => openPopup(paypalPopup));
         cardBtn.addEventListener('click', () => openPopup(cardPopup));
 
-        // Event listeners for close buttons
         closeBtns.forEach(button => {
             button.addEventListener('click', closeAllPopups);
         });
 
-        // Close popup when clicking outside of popup content
         window.addEventListener('click', (event) => {
             if (event.target.classList.contains('popup-container')) {
                 closeAllPopups();
@@ -716,10 +417,6 @@
         });
 
 
-
-
-
-        // Function to toggle the visibility of the password
         function togglePassword(id) {
             const passwordField = document.getElementById(id);
             const type = passwordField.type === 'password' ? 'text' : 'password';
@@ -727,20 +424,19 @@
             toggleIcon.textContent = '🙈';
         }
 
-        // Function to validate if passwords match
         function validatePasswords() {
             const password = document.getElementById('password').value;
             const confirmPassword = document.getElementById('confirm-password').value;
             const errorMessage = document.getElementById('error-message');
 
             if (password !== confirmPassword) {
-                errorMessage.style.display = 'block'; // Show error message
+                errorMessage.style.display = 'block'; 
             } else {
-                errorMessage.style.display = 'none'; // Hide error message
+                errorMessage.style.display = 'none';
             }
         }
 
-    </script>
+    </script> -->
     <script src="https://js.stripe.com/v3/"></script>
     <script src="../assets/JS/aglpaycard.js"></script>
 
