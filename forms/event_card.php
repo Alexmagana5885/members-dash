48f7c99ec253f98fc06effe34268437af81af7f0
Alexmagana5885 <maganaalex634@gmail.com>
2024-10-13 15:08:58 +0300

    commit

--- forms/event_card.php
+++ forms/event_card.php
@@ -185,21 +185,10 @@ if ($result->num_rows > 0) {
     $pdfFileName = $pdfDir . $sanitized_event_name . '_' . $member_email . '.pdf';
 
 
-    $pdf->Output('F', $pdfFileName);
+    $pdf->Output('D', $pdfFileName);
 
 
-    $pdf_relative_path = str_replace('../assets/Documents/EventCards/', '', $pdfFileName);
 
-    $updateQuery = "UPDATE event_registrations 
-                SET invitation_card = '$pdf_relative_path' 
-                WHERE member_email = '$member_email' 
-                AND event_name = '$event_name'";
-
-    if ($conn->query($updateQuery) === TRUE) {
-        echo "PDF generated and stored successfully.";
-    } else {
-        echo "Error updating invitation card: " . $conn->error;
-    }
 } else {
     echo "No event registration found for this member.";
 }
