c30c253beeb6747b4613706308e4f4e9224eb69c
Alexmagana5885 <84841059+Alexmagana5885@users.noreply.github.com>
2024-10-18 08:55:29 +0300

    commit

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-// index.php or config.php
-require_once __DIR__ . '/vendor/autoload.php'; // Ensure autoload is loaded
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // Specify the root directory
-$dotenv->load(); // Load the .env file
+// require_once __DIR__ . '/vendor/autoload.php';
 
-$passkey = getenv('MPESA_PASSKEY');
-$BusinessShortCode = getenv('MPESA_BUSINESS_SHORTCODE');
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
+// $dotenv->load(); 
+
+// $passkey = getenv('MPESA_PASSKEY');
+// $BusinessShortCode = getenv('MPESA_BUSINESS_SHORTCODE');
 
 ?>
 
