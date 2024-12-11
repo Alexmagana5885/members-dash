6cb86b52f170b84068942c6ebb9d50842149cbbb
Alexmagana5885 <maganaalex634@gmail.com>
2024-12-11 15:20:17 +0300

    commit

--- pages/password_reset.php
+++ pages/password_reset.php
@@ -108,7 +108,7 @@
 
         const formData = new FormData(this);
 
-        fetch('forms/PasswordReset.php', {
+        fetch('../forms/PasswordReset.php', {
             method: 'POST',
             body: formData
           })
