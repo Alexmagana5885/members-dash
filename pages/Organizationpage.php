a3dc13cc6294359551473932a3828b0e5c114129
Alexmagana5885 <maganaalex634@gmail.com>
2026-02-08 00:38:51 +0300

    commit

--- pages/Organizationpage.php
+++ pages/Organizationpage.php
@@ -24,6 +24,7 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'member';
     <link href="../assets/img/favicon.png" rel="icon">
     <link href="../assets/img/favicon.png" rel="favicon.png">
     <link rel="stylesheet" href="../assets/CSS/AGLADMIN.css">
+    <link rel="stylesheet" href="../assets/CSS/Dash_Board.css">
 
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet" /> -->
